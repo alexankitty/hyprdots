e79a6d934acb7ab8ca9031e0f779f77d7e256c16
Alexandra <alexankitty@gmail.com>
2026-07-23 04:43:50 -0600

    fix walker transparency getting applied to its shadow

--- .config/hypr/config/rules/layer.lua
+++ .config/hypr/config/rules/layer.lua
@@ -1,4 +1,4 @@
 hl.layer_rule({ match = { namespace = "hyprpicker" }, no_anim = true })
 hl.layer_rule({ match = { namespace = "wayle-bar-.*" }, blur = true, ignore_alpha = 0.3 })
 hl.layer_rule({ match = { namespace = "wayle-notification-popup" }, blur = true, ignore_alpha = 0.3 })
-hl.layer_rule({ match = { namespace = "walker" }, blur = true, ignore_alpha = 0.3 } )
+hl.layer_rule({ match = { namespace = "walker" }, blur = true, ignore_alpha = 0.65 })
